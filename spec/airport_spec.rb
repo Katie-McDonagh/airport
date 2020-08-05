17b666e2004f42f7e20e04da0097ef8886088993
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-05 12:32:57 +0100

    airport creates new instance of plane upon landing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,9 @@ describe Airport do
   it 'responds to land method with one argument' do
    expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'lands a plane object at the airport' do 
+    plane = Plane.new
+    expect(subject.land(plane)).to be_an_instance_of(Plane)
+  end
 end
